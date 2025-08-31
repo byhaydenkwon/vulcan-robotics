ed95abc25227e63a9e842a11a4cecef86cfb7392
undonepotato <icyseas@outlook.com>
2025-08-31 11:02:17 -0500

    feat(hopper): :memo: Add logging to Hopper

--- src/mechanisms/hopper.py
+++ src/mechanisms/hopper.py
@@ -4,25 +4,38 @@ Contains the Hopper class.
 
 from vex import *
 
+from display import Logger, NullLogger
+
 
 class Hopper:
-    def __init__(self, motor: Motor, block_degrees: int, velocity_percent=100) -> None:
+    def __init__(
+        self,
+        motor: Motor,
+        block_degrees: int,
+        velocity_percent=100,
+        logger: Logger | NullLogger = NullLogger(),
+    ) -> None:
         self._motor = motor
         self.velocity = velocity_percent
         self.block_degrees = block_degrees
         # number of degrees required for one block to output
         # maybe improve by sensors later
+        self.logger = logger
 
     def intake(self):
         self._motor.spin(FORWARD)
+        self.logger.log(__name__, "Starting hopper intake")
 
     def output(self, blocks: int) -> None:
         self._motor.spin_for(
             FORWARD, self.block_degrees * blocks, DEGREES, self.velocity, PERCENT, True
         )
+        self.logger.log(__name__, f"Hopper releasing {blocks} blocks")
 
     def flush(self) -> None:
         self._motor.spin(REVERSE)
+        self.logger.log(__name__, "Hopper flushing")
 
     def stop(self) -> None:
         self._motor.stop(HOLD)
+        self.logger.log(__name__, "Hopper stopping")
