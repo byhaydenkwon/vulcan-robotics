edec6e4446ca41b5d4a3f2937858d4aa1b287af4
undonepotato <icyseas@outlook.com>
2025-09-11 13:52:04 -0500

    fix: Update motor ports

--- src/config.py
+++ src/config.py
@@ -7,25 +7,25 @@ from vex import *
 brain = Brain()
 
 # Drivetrain motors
-front_right = Motor(Ports.PORT1, GearSetting.RATIO_6_1, False)
-middle_right = Motor(Ports.PORT2, GearSetting.RATIO_6_1, False)
-back_right = Motor(Ports.PORT3, GearSetting.RATIO_6_1, False)
-front_left = Motor(Ports.PORT4, GearSetting.RATIO_6_1, True)
-middle_left = Motor(Ports.PORT5, GearSetting.RATIO_6_1, True)
-back_left = Motor(Ports.PORT6, GearSetting.RATIO_6_1, True)
+front_right = Motor(Ports.PORT13, GearSetting.RATIO_6_1, False)
+middle_right = Motor(Ports.PORT12, GearSetting.RATIO_6_1, False)
+back_right = Motor(Ports.PORT20, GearSetting.RATIO_6_1, False)
+front_left = Motor(Ports.PORT18, GearSetting.RATIO_6_1, True)
+middle_left = Motor(Ports.PORT19, GearSetting.RATIO_6_1, True)
+back_left = Motor(Ports.PORT11, GearSetting.RATIO_6_1, True)
 
 # Auxillary motors
-hopper = Motor(Ports.PORT11)  # half motor
-intake_bottom = Motor(Ports.PORT12, GearSetting.RATIO_6_1, False)
-intake_top = Motor(Ports.PORT13)  # half motor
+hopper = Motor(Ports.PORT1)  # half motor
+intake_bottom = Motor(Ports.PORT10, GearSetting.RATIO_6_1, False)
+intake_top = Motor(Ports.PORT2)  # half motor
 
 # Odometry
 # Basic (one-wheel)
 # tracking_wheel = Rotation(Ports.PORT14)
 
 # Standard (two-wheel)
-parallel_tracking = Rotation(Ports.PORT14)  # parallel to drive wheels
-perpendicular_tracking = Rotation(Ports.PORT15)  # perpendicular to drive wheels
+parallel_tracking = Rotation(Ports.PORT3)  # parallel to drive wheels
+perpendicular_tracking = Rotation(Ports.PORT4)  # perpendicular to drive wheels
 
 # Advanced (three-wheel)
 # left_tracking = Rotation(Ports.PORT14)
@@ -33,10 +33,10 @@ perpendicular_tracking = Rotation(Ports.PORT15)  # perpendicular to drive wheels
 # back_tracking = Rotation(Ports.PORT16)
 
 # Sensors
-inertial_sensor = Inertial(Ports.PORT7)
-optical_sensor = Optical(Ports.PORT8)
-gps_sensor = Gps(Ports.PORT9, 0, 0, DistanceUnits.MM, 180)
-block_color_sensor = Optical(Ports.PORT10)
+inertial_sensor = Inertial(Ports.PORT5)
+optical_sensor = Optical(Ports.PORT6)
+gps_sensor = Gps(Ports.PORT7, 0, 0, DistanceUnits.MM, 180)
+block_color_sensor = Optical(Ports.PORT8)
 
 # Tube intake (pneumatic)
 henry = DigitalOut(brain.three_wire_port.a)
