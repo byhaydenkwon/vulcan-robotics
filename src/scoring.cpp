848af1ef5c12d3c28196f3f881b187972de2bb68
undonepotato <icyseas@outlook.com>
2025-12-04 22:36:02 -0600

    feat(scoring): :sparkles: Fully implement scoring

--- src/scoring.cpp
+++ src/scoring.cpp
@@ -1,81 +1,135 @@
-// The scoring mechanism.
+/// The scoring mechanism.
+#include "scoring.hpp"
+
 #include <array>
+#include <cmath>
 
 #include "lemlib/api.hpp"
 #include "main.h"
 
-// A stack-based intake control class.
-class Scoring {
-   public:
-    enum class ScoreTarget { Low, Middle, High, Count };
+void Scoring::start_control_loop() {
+    pros::Task scoring_task([this] { this->control_loop(); },
+                            TASK_PRIORITY_DEFAULT, TASK_STACK_DEPTH_DEFAULT,
+                            "intake");
+}
 
-    Scoring(pros::Motor top_motor, pros::Motor middle_motor,
-            pros::Motor intake_motor, pros::Motor hopper_motor)
-        : top_{top_motor},
-          middle_{middle_motor},
-          intake_{intake_motor},
-          hopper_{hopper_motor} {
-        active_state_list_.reserve(static_cast<int>(State::Count));
-    }
+Scoring::State Scoring::get_state_for_score_target(
+    Scoring::ScoreTarget target) {
+    return target_states_[static_cast<int>(target)];
+}
 
-    void intake() { pushActiveState(State::Intaking); }
-    void flush() { pushActiveState(State::Flushing); }
-    void score(ScoreTarget target) {
-        pushActiveState(getStateForScoreTarget(target));
-    }
-
-    void stopIntaking() { removeAllStates(State::Intaking); }
-    void stopFlushing() { removeAllStates(State::Flushing); }
-    void stopScoring(ScoreTarget target) {
-        removeAllStates(getStateForScoreTarget(target));
-    }
-
-   private:
-    enum class State {
-        Intaking,
-        ScoringLow,
-        ScoringMiddle,
-        ScoringHigh,
-        Flushing,
-        Count
-    };
-
-    State getStateForScoreTarget(ScoreTarget target) {
-        return target_states[static_cast<int>(target)];
-    }
-
-    void pushActiveState(State state) {
-        state_list_mutex_.take(200);
+void Scoring::push_active_state(Scoring::State state) {
+    if (state_list_mutex_.take(200)) {
+        // * we don't risk stopping the entire robot
         active_state_list_.push_back(state);
         state_list_mutex_.give();
+    } else {
+        printf("STATE LIST MUTEX TIMED OUT WHILE PUSHING STATE\n");
     }
+}
 
-    void removeAllStates(State state) {
-        state_list_mutex_.take(200);
-        active_state_list_.erase(std::remove(active_state_list_.begin(),
-                                             active_state_list_.end(), state),
-                                 active_state_list_.end());
+void Scoring::remove_all_states(Scoring::State state) {
+    if (state_list_mutex_.take(200)) {
+        std::erase(active_state_list_, state);
         state_list_mutex_.give();
+    } else {
+        printf("STATE LIST MUTEX TIMED OUT WHILE REMOVING STATE");
+    }
+}
+
+void Scoring::control_loop() {
+    while (!stop_next_) {
+        State active{State::Idle};
+
+        if (state_list_mutex_.take(200)) {
+            if (!active_state_list_.empty()) {
+                active = active_state_list_.back();
+            }
+            state_list_mutex_.give();
+        } else {
+            printf("STATE LIST MUTEX TIMED OUT IN CONTROL LOOP");
+        }
+
+        switch (active) {
+            case State::Intaking:
+                spin_intake();
+                break;
+            case State::Flushing:
+                spin_flush();
+                break;
+            case State::ScoringLow:
+                spin_score_low();
+                break;
+            case State::ScoringMiddle:
+                spin_score_middle();
+                break;
+            case State::ScoringHigh:
+                spin_score_high();
+                break;
+            default:
+                spin_stop();
+        }
+
+        pros::Task::delay(25);
+    }
+}
+
+void Scoring::spin_stop() {
+    top_.brake();
+    middle_.brake();
+    intake_.brake();
+    hopper_.brake();
+}
+void Scoring::spin_intake() {
+    top_.brake();
+    spin_motor_percent(middle_, 100);
+    spin_motor_percent(intake_, 100);
+    spin_motor_percent(hopper_, 100);
+}
+void Scoring::spin_flush() {
+    spin_motor_percent(top_, -100);
+    spin_motor_percent(middle_, 100);
+    spin_motor_percent(intake_, 100);
+    spin_motor_percent(hopper_, -100);
+}
+void Scoring::spin_score_high() {
+    spin_motor_percent(top_, 100);
+    spin_motor_percent(middle_, 100);
+    intake_.brake();
+    spin_motor_percent(hopper_, -100);
+}
+void Scoring::spin_score_middle() {
+    spin_motor_percent(top_, -100);
+    spin_motor_percent(middle_, 100);
+    intake_.brake();
+    spin_motor_percent(hopper_, -100);
+}
+void Scoring::spin_score_low() {
+    top_.brake();
+    spin_motor_percent(middle_, -100);
+    spin_motor_percent(intake_, -100);
+    spin_motor_percent(hopper_, -100);
+}
+
+/// Spin a `pros::Motor` based on percentage of max velocity, rounded up to
+/// the nearest RPM.
+static void spin_motor_percent(const pros::Motor& motor, float percent) {
+    int max_rpm;
+    const pros::MotorGears gearset{motor.get_gearing()};
+
+    switch (gearset) {
+        case pros::MotorGears::ratio_36_to_1:
+            max_rpm = 100;
+            break;
+        case pros::MotorGears::ratio_18_to_1:
+            max_rpm = 200;
+            break;
+        case pros::MotorGears::ratio_6_to_1:
+            max_rpm = 600;
+            break;
+        default:
+            max_rpm = 200;
     }
 
-    static constexpr std::array<State, static_cast<int>(ScoreTarget::Count)>
-        target_states{State::ScoringLow, State::ScoringMiddle,
-                      State::ScoringHigh};
-
-    void control_loop() {}
-
-    void spin_score_intake() {}
-    void spin_score_flush() {}
-    void spin_score_low() {}
-    void spin_score_middle() {}
-    void spin_score_high() {}
-
-    pros::Motor top_;
-    pros::Motor middle_;
-    pros::Motor intake_;
-    pros::Motor hopper_;
-
-    std::vector<State> active_state_list_{};  // reserved to state count
-    pros::Mutex state_list_mutex_;
-    bool stop_next_{false};
-};
+    motor.move_velocity(std::ceil((percent / 100.0) * max_rpm));
+}
---
+++ src/scoring.hpp
@@ -0,0 +1,79 @@
+#pragma once
+
+#include <array>
+
+#include "lemlib/api.hpp"
+#include "main.h"
+
+/// A stack-based intake control class.
+class Scoring {
+   public:
+    enum class ScoreTarget { Low, Middle, High, InvalidCount };
+
+    Scoring(pros::Motor top_motor, pros::Motor middle_motor,
+            pros::Motor intake_motor, pros::Motor hopper_motor)
+        : top_{top_motor},
+          middle_{middle_motor},
+          intake_{intake_motor},
+          hopper_{hopper_motor} {
+        active_state_list_.reserve(static_cast<int>(State::InvalidCount));
+    }
+
+    void start_control_loop();
+    void stop_control_loop() { stop_next_ = true; }
+
+    void intake() { push_active_state(State::Intaking); }
+    void flush() { push_active_state(State::Flushing); }
+    void score(ScoreTarget target) {
+        push_active_state(get_state_for_score_target(target));
+    }
+
+    void stop_intaking() { remove_all_states(State::Intaking); }
+    void stop_flushing() { remove_all_states(State::Flushing); }
+    void stop_scoring(ScoreTarget target) {
+        remove_all_states(get_state_for_score_target(target));
+    }
+
+   private:
+    enum class State {
+        Idle,
+        Intaking,
+        ScoringLow,
+        ScoringMiddle,
+        ScoringHigh,
+        Flushing,
+        InvalidCount
+    };
+
+    State get_state_for_score_target(ScoreTarget target);
+
+    static constexpr std::array<State,
+                                static_cast<int>(ScoreTarget::InvalidCount)>
+        target_states_{State::ScoringLow, State::ScoringMiddle,
+                       State::ScoringHigh};
+
+    void push_active_state(State state);
+    void remove_all_states(State state);
+
+    void control_loop();
+
+    void spin_stop();
+    void spin_intake();
+    void spin_flush();
+    void spin_score_high();
+    void spin_score_middle();
+    void spin_score_low();
+
+    static void spin_motor_percent(pros::Motor motor, int percent);
+
+    pros::Motor top_;
+    pros::Motor middle_;
+    pros::Motor intake_;
+    pros::Motor hopper_;
+
+    // technically not a stack, but close!
+    // elements are removed from the middle of the list
+    std::vector<State> active_state_list_{};  // reserved to state count
+    pros::Mutex state_list_mutex_;
+    bool stop_next_{false};
+};
