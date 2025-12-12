7d3cd6dbab55ea7ca056c2add4153c0726017464
undonepotato <icyseas@outlook.com>
2025-12-12 13:45:47 -0600

    fix(auto): :zap: Autonomous tuning distance left

--- src/autonomous_control.cpp
+++ src/autonomous_control.cpp
@@ -15,6 +15,10 @@ void AutonomousControl::match_right() {
     scoring_.stop_intaking();
     loader_.retract();
     scoring_.score(Scoring::ScoreTarget::High);
+    pros::delay(3000);
+    chassis_.moveToPoint(-23.72, 26, 2000, {.forwards = false}, false);
+    chassis_.moveToPoint(-23.72, 32.736756, 2000, {}, false);
+    // wing_.extend();
 }
 
 void AutonomousControl::match_left() {
@@ -22,13 +26,16 @@ void AutonomousControl::match_left() {
     chassis_.moveToPose(10.028, 33.942, 270, 2300, {}, false);
     loader_.extend();
     pros::delay(500);
-    chassis_.moveToPoint(-1.528, 33.942, 1000, {}, false);
+    chassis_.moveToPoint(-1.528, 28.942, 1000, {}, false);
     scoring_.intake();
     pros::delay(1500);
-    chassis_.moveToPoint(23.72, 31.942, 2000, {.forwards = false}, false);
+    chassis_.moveToPoint(23.72, 32.73675, 2000, {.forwards = false}, false);
     scoring_.stop_intaking();
     loader_.retract();
     scoring_.score(Scoring::ScoreTarget::High);
+    chassis_.moveToPoint(23.72, 26, 2000, {.forwards = false}, false);
+    chassis_.moveToPoint(23.72, 32.736756, 2000, {}, false);
+    // wing_.extend();
 }
 
 void AutonomousControl::skills() {
--- src/autonomous_control.hpp
+++ src/autonomous_control.hpp
@@ -7,8 +7,9 @@
 class AutonomousControl {
    public:
     AutonomousControl(lemlib::Chassis& chassis, Scoring& scoring,
-                      pros::adi::Pneumatics& loader)
-        : chassis_(chassis), scoring_(scoring), loader_(loader) {};
+                      pros::adi::Pneumatics& loader,
+                      pros::adi::Pneumatics& wing)
+        : chassis_(chassis), scoring_(scoring), loader_(loader), wing_{wing} {};
     void match_right();
     void match_left();
     void skills();
@@ -17,4 +18,5 @@ class AutonomousControl {
     lemlib::Chassis& chassis_;
     Scoring& scoring_;
     pros::adi::Pneumatics& loader_;
+    pros::adi::Pneumatics& wing_;
 };
