cb66426c286abb6a01389a1af0f82432bebb5ffa
undonepotato <icyseas@outlook.com>
2026-01-12 22:34:43 -0600

    feat(config): :sparkles: Add field quadrant to global settings

--- src/utils/settings.hpp
+++ src/utils/settings.hpp
@@ -13,9 +13,17 @@ class Settings {
 
     enum class RunType { Test, Scrimmage, Match, InvalidCount };
     enum class SkillsType { None, Driver, Autonomous, InvalidCount };
+    enum class FieldQuadrant {
+        RedUpper,
+        RedLower,
+        BlueUpper,
+        BlueLower,
+        InvalidCount
+    };
 
     RunType run_type{RunType::Test};
     SkillsType skills_type{SkillsType::None};
+    FieldQuadrant field_quadrant{FieldQuadrant::RedUpper};
 
     bool try_win_point{false};
     bool use_experimental_code{false};
