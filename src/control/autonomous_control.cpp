aea934f48473593932bcc75d3fb04d80e47f6ffe
undonepotato <icyseas@outlook.com>
2026-01-16 09:37:43 -0600

    feat(auto): :sparkles: Add candidate skills code

--- src/control/autonomous_control.cpp
+++ src/control/autonomous_control.cpp
@@ -46,9 +46,29 @@ void AutonomousControl::match_left() {
 }
 
 void AutonomousControl::skills() {
-    scoring_.score(Scoring::ScoreTarget::Low);
-    chassis_.moveToPoint(0, 10, 2000, {.forwards = true}, false);
-    chassis_.moveToPoint(0, -25, 2000, {.forwards = false, .maxSpeed = 50},
-                         false);
-    chassis_.moveToPoint(0, -100, 10000, {.forwards = false}, false);
+    // right-side 4 blocks + long goal control auto
+    chassis_.setPose(0, 0, 0);
+    chassis_.moveToPose(-10.028, 33.942, 90, 2300, {}, false);
+    loader_.extend();
+    pros::delay(500);
+    chassis_.moveToPoint(8.028, 29.942, 1000, {.maxSpeed = 80.0}, false);
+    scoring_.intake();
+    pros::delay(700);
+    chassis_.moveToPoint(-27.72, 31.0, 2000, {.forwards = false}, false);
+    scoring_.stop_intaking();
+    loader_.retract();
+    scoring_.score(Scoring::ScoreTarget::High);
+    pros::delay(2500);
+    chassis_.moveToPose(-15.0, 14.0, 270, 2300, {}, false);
+    scoring_.stop_scoring(Scoring::ScoreTarget::High);
+    wing_.extend();
+    pros::delay(200);
+    wing_.retract();
+    chassis_.moveToPoint(-45.0, 25.0, 2300, {}, false);
+    chassis_.moveToPoint(-15.0, 25.0, 2300, {.forwards = false}, false);
+    wing_.retract();
+    // get out any extra blocks in order to ensure control
+
+    // go clear other loader
+    // chassis_.moveToPose(float x, float y, float theta, int timeout)
 }
