dd0ebc5e821c18b9c9e428f9b77f742b2f7ab64f
undonepotato <icyseas@outlook.com>
2026-01-16 11:29:22 -0600

    feat(auto): :sparkles: Add candidate right-lower-goal scoring auto

--- src/control/autonomous_control.cpp
+++ src/control/autonomous_control.cpp
@@ -5,27 +5,42 @@
 
 void AutonomousControl::match_right() {
     chassis_.setPose(0, 0, 0);
-    chassis_.moveToPose(-10.028, 33.942, 90, 2300, {}, false);
+
+    // get three blocks from loader
+    chassis_.moveToPose(-10.028, 33.942, 90, 2000, {}, false);
     loader_.extend();
-    pros::delay(500);
+    pros::delay(300);
     chassis_.moveToPoint(8.028, 29.942, 1000, {.maxSpeed = 80.0}, false);
     scoring_.intake();
-    pros::delay(700);
-    chassis_.moveToPoint(-27.72, 31.0, 2000, {.forwards = false}, false);
+    pros::delay(700);  // three blocks, most of the time (four if not)
+
+    // move to goal; score four blocks
+    chassis_.moveToPoint(-27.72, 31.0, 2000, {.forwards = false}, true);
+    scoring_.score(Scoring::ScoreTarget::High);
     scoring_.stop_intaking();
     loader_.retract();
-    scoring_.score(Scoring::ScoreTarget::High);
-    pros::delay(1500);
+    chassis_.waitUntilDone();
+    pros::delay(1000);
+
+    // wing/long goal control sequence
+    wing_.extend();
     chassis_.moveToPose(-15.0, 14.0, 270, 2300, {}, false);
     scoring_.stop_scoring(Scoring::ScoreTarget::High);
-    wing_.extend();
+    wing_.retract();
+    chassis_.moveToPoint(-45.0, 24.0, 2000,
+                         {.minSpeed = 80, .earlyExitRange = 7}, false);
+    chassis_.moveToPoint(-15.0, 25.0, 2000, {.forwards = false}, false);
+    wing_.retract();
+
+    // get three blocks in middle
+    scoring_.intake();
+    chassis_.turnToHeading(220, 1000);
+    chassis_.moveToPoint(-28.0, 8.0, 1000, {.maxSpeed = 70.0}, false);
     pros::delay(200);
-    wing_.retract();
-    chassis_.moveToPoint(-45.0, 24.0, 2300, {}, false);
-    chassis_.moveToPose(-15.0, 25.0, 270, 2300, {.forwards = false}, false);
-    wing_.retract();
-    // scoring_.intake();
-    // chassis_.moveToPose()
+
+    // score in lower
+    chassis_.moveToPose(-36.0, 0.0, 220, 700, {}, false);
+    scoring_.score(Scoring::ScoreTarget::Low);
 }
 
 void AutonomousControl::match_left() {
