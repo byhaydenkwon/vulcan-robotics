4be6cf36c9148759ceabe448c9fa60b3b48e3ec8
undonepotato <icyseas@outlook.com>
2026-01-17 00:01:53 -0600

    feat(auto): :ambulance: Implement incredibly complex skills auto
    Extend double park

--- src/control/autonomous_control.cpp
+++ src/control/autonomous_control.cpp
@@ -60,29 +60,37 @@ void AutonomousControl::match_left() {
 }
 
 void AutonomousControl::skills() {
-    // right-side 4 blocks + long goal control auto
-    chassis_.setPose(0, 0, 0);
-    chassis_.moveToPose(-10.028, 33.942, 90, 2300, {}, false);
-    loader_.extend();
-    pros::delay(500);
-    chassis_.moveToPoint(8.028, 29.942, 1000, {.maxSpeed = 80.0}, false);
-    scoring_.intake();
-    pros::delay(700);
-    chassis_.moveToPoint(-27.72, 31.0, 2000, {.forwards = false}, false);
-    scoring_.stop_intaking();
-    loader_.retract();
-    scoring_.score(Scoring::ScoreTarget::High);
-    pros::delay(2500);
-    chassis_.moveToPose(-15.0, 14.0, 270, 2300, {}, false);
-    scoring_.stop_scoring(Scoring::ScoreTarget::High);
-    wing_.extend();
-    pros::delay(200);
-    wing_.retract();
-    chassis_.moveToPoint(-45.0, 25.0, 2300, {}, false);
-    chassis_.moveToPoint(-15.0, 25.0, 2300, {.forwards = false}, false);
-    wing_.retract();
-    // get out any extra blocks in order to ensure control
+    double_park_.extend();
+    // // right-side 4 blocks + long goal control auto
+    // chassis_.setPose(0, 0, 0);
 
-    // go clear other loader
-    // chassis_.moveToPose(float x, float y, float theta, int timeout)
+    // // get three blocks from loader
+    // chassis_.moveToPose(-10.028, 33.942, 90, 2000, {}, false);
+    // loader_.extend();
+    // pros::delay(300);
+    // chassis_.moveToPoint(10.528, 29.942, 1000, {.maxSpeed = 80.0}, false);
+    // scoring_.intake();
+    // pros::delay(700);  // three blocks, most of the time (four if not)
+
+    // // move to goal; score four blocks
+    // chassis_.moveToPoint(-27.72, 31.0, 2000, {.forwards = false}, false);
+    // scoring_.score(Scoring::ScoreTarget::High);
+    // scoring_.stop_intaking();
+    // loader_.retract();
+    // pros::delay(2000);
+
+    // // wing/long goal control sequence
+    // wing_.extend();
+    // chassis_.moveToPose(-15.0, 13.0, 270, 2300, {}, false);
+    // scoring_.stop_scoring(Scoring::ScoreTarget::High);
+    // wing_.retract();
+    // chassis_.moveToPoint(-45.0, 24.5, 2000,
+    //                      {.minSpeed = 80, .earlyExitRange = 4}, false);
+    // chassis_.moveToPoint(-15.0, 25.0, 2000, {.forwards = false}, false);
+    // wing_.retract();
+
+    // // go clear other loader
+    // chassis_.moveToPoint(-10, 42, 4000);
+    // chassis_.moveToPoint(-100, 50, 10000);
+    // chassis_.moveToPose(-121.403, 36, 270, 7000);
 }
